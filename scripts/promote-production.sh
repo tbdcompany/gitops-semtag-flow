8a17456118d73262df7fe25ac2f92ad7ddeacac4
Nick Stogner <nicholas.stogner@gmail.com>
2022-08-11 11:48:00 -0400

    Update

--- scripts/promote-production.sh
+++ scripts/promote-production.sh
@@ -5,13 +5,13 @@ FORPRD_REGEX="^promote\/prd$"
 GA_SEMVER_REGEX="^v[0-9]\+\.[0-9]\+\.[0-9]\+$"
 
 print_exit () {
-	echo $1
-	exit 1
+	echo $2
+	exit $1
 }
 
-git tag --no-contains HEAD | grep -q $PRD_REGEX       || print_exit "Production tag already exists on commit, exiting early."
-git tag --contains HEAD    | grep -q $FORPRD_REGEX    || print_exit "Production request promotion tag not specified, exiting early."
-git tag --contains HEAD    | grep -q $GA_SEMVER_REGEX || print_exit "Missing GA semver tag on the same commit, not promoting."
+git tag --no-contains HEAD | grep -q $PRD_REGEX       || print_exit 0 "Production tag already exists on commit, exiting early."
+git tag --contains HEAD    | grep -q $FORPRD_REGEX    || print_exit 0 "Production request promotion tag not specified, exiting early."
+git tag --contains HEAD    | grep -q $GA_SEMVER_REGEX || print_exit 1 "Missing GA semver tag on the same commit, not promoting."
 
 git tag --force prd
 git push --force origin prd
